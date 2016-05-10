537e0479a1f8c7f6e25d550eb4c9e6d5e0eef3a7
isaac0977 <minkyu_suh@brown.edu>
2016-05-10 02:22:57 -0400

    prevent duplicate

--- client/index.js
+++ client/index.js
@@ -8,11 +8,15 @@ import './index.html';
 
 if (Meteor.isClient) {
   $(document).ready( function() {
-    Actions.insert({"category": "Transportation", "text": "Ride a bike", "carbon": 3, "money": 2},
-      {"category": "Home", "text": "Turn off the lights", "carbon": 3, "money": 1},
-      {"category": "Transportation", "text": "Walk more", "carbon": 6, "money": 4},
-      {"category": "Home", "text": "Don't eat red meat", "carbon": 5, "money": 8},
-      {"category": "Recycling", "text": "Recycle pl. bottles", "carbon": 4, "money": 5})
+    var included = Events.findOne({"text": "Ride a bike"})
+      if (included === undefined) {
+        Actions.insert({"category": "Transportation", "text": "Ride a bike", "carbon": 3, "money": 2},
+                        {"category": "Home", "text": "Turn off the lights", "carbon": 3, "money": 1},
+                        {"category": "Transportation", "text": "Walk more", "carbon": 6, "money": 4},
+                        {"category": "Home", "text": "Don't eat red meat", "carbon": 5, "money": 8},
+                          {"category": "Recycling", "text": "Recycle pl. bottles", "carbon": 4, "money": 5})
+      }
+    
   })
     
     Template.register.events({
